f601229a5afee775535cd696661b0a4684388f56
Mary Monchari Gichana <marygichana95@gmail.com>
2024-09-30 09:08:08 +0300

    Adds speed.js

--- speed.js
+++ speed.js
@@ -1,10 +1,13 @@
+// Function to check the speed and calculate demerit points
 function checkSpeed(speed) {
+  // Define the speed limit and demerit points factor
   const speedLimit = 70;
   const demeritPointsPerKm = 5;
 
   if (speed <= speedLimit) {
     console.log("Ok");
   } else {
+    // Calculate how many demerit points based on the excess speed
     const demeritPoints = Math.floor((speed - speedLimit) / demeritPointsPerKm);
     if (demeritPoints > 12) {
       console.log("License suspended");
@@ -14,5 +17,6 @@ function checkSpeed(speed) {
   }
 }
 
-const speed = 120;
+//check speed with a value
+const speed = 200;
 checkSpeed(speed);
